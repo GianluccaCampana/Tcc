3598a787f6c83d13663b47baf0796757ea3c06f0
GianluccaCampanaGianluccaCampana <gianluccacampana19@gmail.com>
2019-10-06 12:21:33 -0300

    ajustando detalhes

--- Blacaleletrica/app/src/main/java/com/example/blacaleletrica/Luminaria.java
+++ Blacaleletrica/app/src/main/java/com/example/blacaleletrica/Luminaria.java
@@ -3,16 +3,20 @@ package com.example.blacaleletrica;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.AppCompatRadioButton;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.RadioGroup;
+import android.widget.Toast;
 
 import static com.example.blacaleletrica.R.layout.activity_luminaria;
 
 public class Luminaria extends AppCompatActivity {
 
+
+    // declarando variáveis
    private EditText lar,comp,resul;
    private Button calLum;
    private RadioGroup ambienteL;
@@ -23,7 +27,8 @@ public class Luminaria extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(activity_luminaria);
 
-         lar=(EditText)findViewById(R.id.editTextComprimentoLum);
+        // Ligando variáveis com xml
+         lar=(EditText)findViewById(R.id.editTextLargLum);
          comp=(EditText) findViewById(R.id.editTextComprimentoLum);
          resul=(EditText)findViewById(R.id.editTextResulLum);
          calLum=(Button)findViewById(R.id.buttonCalcularLum);
@@ -32,29 +37,73 @@ public class Luminaria extends AppCompatActivity {
         calLum.setOnClickListener(new View.OnClickListener() {
              @Override
              public void onClick(View v) {
-                 int ambL = ambienteL.getCheckedRadioButtonId();
+                 int ambL = ambienteL.getCheckedRadioButtonId();// ligandos com os radios
 
-                 double numb1=Double.parseDouble(lar.getText().toString());
-                 double numb2=Double.parseDouble(comp.getText().toString());
+                 double numb1= 0;
+
+                 double numb2= 0;
 
 
-                 if(ambL == R.id.radioButtonSala){
+                 if(numb1 == 0 || numb2 == 0){
+
+                     // Caso falte algum valor em largura ou comprimento irá aparecer mensagem de alerta
+                     Context alerta = getApplicationContext();
+                     Toast toast = Toast.makeText(alerta, "Necessário que seja preechido largura e comprimento", Toast.LENGTH_SHORT);
+                     toast.show();
+
+                 }else if(ambL == R.id.radioButtonSala){
+
+                     //convertendo valores digitados para double
+                     numb1 =Double.parseDouble(lar.getText().toString());
+                     numb2 = Double.parseDouble(comp.getText().toString());
                      double calc = numb1 * numb2 ;
                      double lum = 150*calc ;
 
-                     resul.setText(String.valueOf(lum));
+                     // mostrar o resultado o resultado no editText resultado
+                     // Math.round arredonda o resulta pra cima
+                     resul.setText(String.valueOf(Math.round(calc)));
                  }else if(ambL == R.id.radioButtonCozinha){
+                     //convertendo valores digitados para double
+                     numb1 =Double.parseDouble(lar.getText().toString());
+                     numb2 = Double.parseDouble(comp.getText().toString());
+
                      double calc = (numb1 * numb2) * 300;
-                     resul.setText(String.valueOf(calc));
+
+                     // mostrar o resultado o resultado no editText resultado
+                     // Math.round arredonda o resulta pra cima
+                     resul.setText(String.valueOf(Math.round(calc)));
                  }else if(ambL == R.id.radioButtonQuarto){
                      double calc = (numb1 * numb2) * 150;
-                     resul.setText(String.valueOf(calc));
+
+                     // mostrar o resultado o resultado no editText resultado
+                     // Math.round arredonda o resulta pra cima
+                     resul.setText(String.valueOf(Math.round(calc)));
                  }else if(ambL == R.id.radioButtonBanheiro){
+                     //convertendo valores digitados para double
+                     numb1 =Double.parseDouble(lar.getText().toString());
+                     numb2 = Double.parseDouble(comp.getText().toString());
+
                      double calc = (numb1 * numb2) * 150;
-                     resul.setText(String.valueOf(calc));
+
+                     // mostrar o resultado o resultado no editText resultado
+                     // Math.round arredonda o resulta pra cima
+                     resul.setText(String.valueOf(Math.round(calc)));
                  }else if(ambL == R.id.radioButtonGeral){
+                     //convertendo valores digitados para double
+                     numb1 =Double.parseDouble(lar.getText().toString());
+                     numb2 = Double.parseDouble(comp.getText().toString());
+
                      double calc = (numb1 * numb2) * 100;
-                     resul.setText(String.valueOf(calc));
+
+                     // mostrar o resultado o resultado no editText resultado
+                     // Math.round arredonda o resulta pra cima
+                     resul.setText(String.valueOf(Math.round(calc)));
+                 }else{
+
+                     // caso nenhum cômodo seja selecionado aparecerá uma mensagem de alerta com toast
+                     Context alerta= getApplicationContext();
+                     Toast toast = Toast.makeText(alerta,"Nenhum cômodo selecionado", Toast.LENGTH_SHORT);
+                     toast.show();
                  }
 
              }
--- Blacaleletrica/app/src/main/java/com/example/blacaleletrica/Tomadas.java
+++ Blacaleletrica/app/src/main/java/com/example/blacaleletrica/Tomadas.java
@@ -3,14 +3,18 @@ package com.example.blacaleletrica;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.service.autofill.FieldClassification;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.RadioGroup;
+import android.widget.Toast;
 
 public class Tomadas extends AppCompatActivity {
 
+    // Delclaração de viriáveis
     EditText comp, lar, resul;
     Button cal;
     RadioGroup grupo;
@@ -20,6 +24,7 @@ public class Tomadas extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tomadas);
 
+        // Conectando elas com XML
         comp =(EditText)findViewById(R.id.editTextComprimentoTom);
         lar = (EditText)findViewById(R.id.editTextLarguraTom);
         resul=(EditText)findViewById(R.id.editTextResulTom);
@@ -30,27 +35,62 @@ public class Tomadas extends AppCompatActivity {
         cal.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                int op = grupo.getCheckedRadioButtonId();
-
-                double numb1 = Double.parseDouble(comp.getText().toString());
-                double numb2= Double.parseDouble(lar.getText().toString());
+                int op = grupo.getCheckedRadioButtonId(); //conectando os radios
 
 
-                if(op == R.id.radioButton1) {
+                // declaração de variáveis
+                double numb1 = 0;
+
+                double numb2= 0;
+
+
+
+                // Verificando a id dos radios, para ai sim efetuar os cálculos
+                 if(numb1 == 0 || numb2 ==0){
+
+                    // Caso falte algum valor em largura ou comprimento irá aparecer mensagem de alerta
+                    Context alerta = getApplicationContext();
+                    Toast toast = Toast.makeText(alerta,"Necessario que largura ou compromento esteja preechido", Toast.LENGTH_SHORT);
+                    toast.show();
+                }else if(op == R.id.radioButton1) {
+
+                    //convertendo valores digitados em double
+                    numb1 = Double.parseDouble(comp.getText().toString());
+                    numb2 = Double.parseDouble(lar.getText().toString());
+
                     double total = ((numb1 * 2) + (numb2 * 2)) / 3.5;
 
-                    resul.setText(String.valueOf(total));
+                    // mostrar o resultado o resultado no editText resultado
+                    // Math.round arredonda o resulta pra cima
+                    resul.setText(String.valueOf(Math.round(total)));
                 }else if(op == R.id.radioButton2){
+                    //convertendo valores digitados em double
+                    numb1 = Double.parseDouble(comp.getText().toString());
+                    numb2 = Double.parseDouble(lar.getText().toString());
+
+
                     double total = ((numb1 * 2) + (numb2 * 2)) / 5;
 
-                    resul.setText(String.valueOf(total));
+                    // mostrar o resultado o resultado no editText resultado
+                    // Math.round arredonda o resulta pra cima
+                    resul.setText(String.valueOf(Math.round(total)));
                 }else if(op == R.id.radioButton3){
+                    //convertendo valores digitados em double
+                    numb1 = Double.parseDouble(comp.getText().toString());
+                    numb2 = Double.parseDouble(lar.getText().toString());
+
+
                     double total =1;
-                    resul.setText(String.valueOf(total));
+
+                    // mostrar o resultado o resultado no editText resultado
+                    // Math.round arredonda o resulta pra cima
+                    resul.setText(String.valueOf(Math.round(total)));
                 }else{
-                    AlertDialog.Builder alert = new AlertDialog.Builder(Tomadas.this);
-                    alert.setTitle("Nenhum comodo selecionado");
-                    alert.setNeutralButton("ok", null);
+
+                    // caso nenhum cômodo seja selecionado aparecerá uma mensagem de alerta com toast
+                    Context alerta = getApplicationContext();
+                    Toast toast = Toast.makeText(alerta, "Nunhum Cômodo selecionado", Toast.LENGTH_LONG);
+                    toast.show();
                 }
 
 
