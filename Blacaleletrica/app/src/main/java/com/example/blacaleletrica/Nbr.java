8dd64a0d6005997b2edc3d79881bee89ea226ee8
ANDREY RAMOS CALDAS <41805071+andreyrehv@users.noreply.github.com>
2019-11-02 21:50:46 -0300

    projeto antigo

--- Blacaleletrica/app/src/main/java/com/example/blacaleletrica/Nbr.java
+++ Blacaleletrica/app/src/main/java/com/example/blacaleletrica/Nbr.java
@@ -1,6 +1,5 @@
 package com.example.blacaleletrica;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.view.MenuItemCompat;
 
@@ -8,88 +7,52 @@ import android.content.ActivityNotFoundException;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.Button;
 import android.widget.SearchView;
 import android.widget.Toast;
 
 import com.github.barteksc.pdfviewer.PDFView;
 import com.github.barteksc.pdfviewer.util.FitPolicy;
-import com.google.android.gms.tasks.OnFailureListener;
-import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.firebase.storage.FirebaseStorage;
-import com.google.firebase.storage.StorageReference;
-import com.google.firebase.storage.UploadTask;
 
 import java.io.File;
 
 public class Nbr extends AppCompatActivity  {
-
-    Button btnupload;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_nbr);
 
-        btnupload = (Button) findViewById(R.id.btnupload);
-
-        upload();
-
-       // pdf();
-
+        pdf();
+        pesquisaPalavras();
 
 
 
     }
 
-    private void upload() {
-        btnupload.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                FirebaseStorage storage = FirebaseStorage.getInstance();
-                StorageReference storageRef = storage.getReferenceFromUrl("gs://blacaleletrica.com");
+    private void pesquisaPalavras() {
+        File pdfFile = new File("path");
+        if(pdfFile.exists())
+        {
+            Uri path = Uri.fromFile(pdfFile);
+            Intent pdfIntent = new Intent(Intent.ACTION_VIEW);
+            pdfIntent.setDataAndType(path, "NBR 5410.pdf");
+            pdfIntent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
 
-                Uri file = Uri.fromFile(new File("data/data/file-path/file-name"));
-                Log.d("file", file.getPath());
-
-
-                StorageReference riversRef = storageRef.child("firebase-storage");
-
-                UploadTask uploadTask = riversRef.putFile(file);
-
-// Register observers to listen for when the download is done or if it fails
-                uploadTask.addOnFailureListener(new OnFailureListener() {
-                    @Override
-                    public void onFailure(@NonNull Exception exception) {
-                        // Handle unsuccessful uploads
-                        Log.d("uploadFail", "" + exception);
-
-                    }
-                }).addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
-                    @Override
-                    public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
-                        // taskSnapshot.getMetadata() contains file metadata such as size, content-type, and download URL.
-                        sendNotification("upload backup", 1);
-
-                        //Uri downloadUrl = taskSnapshot.getDownloadUrl();
-
-                        //Log.d("downloadUrl", "" + downloadUrl);
-                    }
-                });
+            try
+            {
+                startActivity(pdfIntent);
             }
-        });
+            catch(ActivityNotFoundException e)
+            {
+                Toast.makeText(Nbr.this, "No Application available to view pdf", Toast.LENGTH_LONG).show();
+            }
+        }
     }
 
-    private void sendNotification(String upload_backup, int i) {
-    }
 
-
-
-   /* private void pdf() {
+    private void pdf() {
         //PDF View
         PDFView pdfView = findViewById(R.id.pdfView);
         pdfView.fromAsset("NBR 5410.pdf")
@@ -105,7 +68,7 @@ public class Nbr extends AppCompatActivity  {
                 .spacing(0)
                 .pageFitPolicy(FitPolicy.WIDTH)
                 .load();
-    }*/
+    }
 
 
 }
